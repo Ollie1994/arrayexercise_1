51e67a45579a5f04fa0f0ea3fab43de822864ce0
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-20 13:31:54 +0200

    Uppgift klar!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // int[] integers = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10};
+        int[] intergers = new int[10];
+        for (int i = 0; i < intergers.length; i++) {
+
+            System.out.println("Enter an integer");
+            intergers[i] = readIntergers();
+        }
+        System.out.println(Arrays.toString(intergers));
+        int minAge = findMin(intergers);
+        System.out.println("MinAge = " + minAge);
+
+
+    } //-----------------------METHODS------------------------------------------------------------
+    public static int readIntergers() {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        return input;
     }
+    //------------------------------------------------------------
+    public static int findMin(int[] intergers) {
+        System.out.println(Arrays.toString(intergers));
+        int sum = 0;
+        int minAge = intergers[0];
+        for (int i = 0; i < intergers.length; i++) {
+            sum = intergers[i];
+            System.out.println(intergers[i]);
+            if (minAge > sum) {
+                minAge = sum;
+                System.out.println("Ny min: " + minAge);
+            }
+        }
+
+            return minAge;
+
+    }
+
+
+
+
+
 }
